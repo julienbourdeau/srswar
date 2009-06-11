ba1805863f9013591471f149307ee75fc2e5a380
arnaud.bellec <arnaud.bellec@310a4c36-4f40-11de-bba4-9112670d796e>
2009-06-11 19:12:12 +0000

    Improved javadoc on graphics package

--- branches/gandalf/src/graphics/AttackUnitView.java
+++ branches/gandalf/src/graphics/AttackUnitView.java
@@ -4,8 +4,18 @@ import gamelogic.units.Unit;
 import gui.BoardView;
 import gui.Options;
 
+/**
+ * Draft class which will draw attack units in the future.
+ * @author kbok
+ */
 public class AttackUnitView extends UnitView{
 
+	/**
+	 * Creates a new attack unit's view on the given BoardView and options for the given unit.
+	 * @param m The Unit associated with the view.
+	 * @param board The View of the board the unit belongs to.
+	 * @param o The options of the BoardView.
+	 */
 	public AttackUnitView(Unit m, BoardView board, Options o) {
 		super(m, board, o);
 		// TODO Auto-generated constructor stub
